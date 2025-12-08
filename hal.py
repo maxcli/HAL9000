e7afb52327f75a943db4b29c07a0fa6b28865966
maxcli <petercli@hotmail.com>
2025-12-08 01:41:50 -0800

    new requirements

--- hal.py
+++ hal.py
@@ -1,56 +1,47 @@
-
 #!/usr/bin/env python
 
-# Hal.py The code is based on google-assistant-sdk's hotword.py
-# setup:
-#    create Python requirements : pip freeze > requirements.txt
-#    Install pip3 install -r requirements.txt
+# Hal.py
 
-# run:
-#    python3 hal.py
-
-from __future__ import print_function
+import RPi.GPIO as GPIO
 from gpiozero import LED 
-from pathlib import Path
-from time import sleep
 from signal import signal,SIGTERM,SIGHUP,pause
-from threading import Thread
-from google.assistant.library import Assistant
-from google.assistant.library.event import EventType
-from google.assistant.library.file_helpers import existing_file
-from pixels import pixels
-
+import time
+import threading
 import json
 import os
 import os.path
 import yaml
 import argparse
-import google.auth.transport.requests
-import google.oauth2.credentials
-
-import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 #definitions
 BUTTON = 17
 redled= LED(13)
 blink_on=False
 reading=True
-statusString="unknown"  # make string list
-DEVICE_API_URL = 'https://embeddedassistant.googleapis.com/v1alpha2'
+statusString="unknown"  
 
 #functions
+def button_handler():
+  
+    
+    global blink_on
+    state = GPIO.input(BUTTON)
+    print("button handler.  blink flag:"+str(blink_on) +" state:"  state) 
+    
+    if blink_on:
+        redled.off()
+    else:    
+        redled.blink(0.5,0.5)
 
+    blink_on=not blink_on
 
 #main loop.  runs on schedule
 def main_daemon():
     while reading:
         print('Wakeup  ' )
-        state = GPIO.input(BUTTON)
-        if state:
-            print("off")
-        else:
-            print("on")
+  
+ 
         #print('Distance: ' +  '{:1.2f}'.format( distancesensor.distance) + " m") #speech . replace print with voice messaged      
         sleep(5)
 
@@ -69,7 +60,7 @@ def safe_exit(signum,frame):
     exit(1)
 
 def startup():
-  
+    
     print("red on")
     redled.on()
     sleep(4)
@@ -88,7 +79,8 @@ try:
     settingsDict =get_settings()
     SS_SUBSCRIPTION_KEY=settingsDict["ss_SUBSCRIPTION_KEY"]
     print("sub key"+ SS_SUBSCRIPTION_KEY)
-   
+    
+    button.when_pressed=button_handler
     startup()
 
 
