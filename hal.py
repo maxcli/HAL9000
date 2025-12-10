6ff73d03c63079ad8d3096b80100c9d2cc1fbcdd
maxcli <petercli@hotmail.com>
2025-12-10 01:43:43 -0800

    add wav

--- hal.py
+++ hal.py
@@ -14,6 +14,12 @@ import os
 import os.path
 import yaml
 import argparse
+import pyaudio
+import wave
+import sys
+
+
+
 from time import sleep
 from pathlib import Path
 from threading import Thread
@@ -26,7 +32,10 @@ redled= LED(13)
 blink_on=False
 reading=True
 statusString="unknown"  
-
+# length of data to read.
+chunk = 1024
+RESPEAKER_INDEX = 1
+INTROWAV="..\voice_dataset\wavs\lj0011.wav"
 #functions
 
 
@@ -44,6 +53,32 @@ def main_daemon():
 
         sleep(2)
 
+def play_wav(wavename):
+    print(f"Hello, my name is {wavename}")
+    # open the file for reading.
+    wf = wave.open(wavename, 'rb')
+
+    # create an audio object
+    p = pyaudio.PyAudio()
+
+    # open stream based on the wave object which has been input.
+    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = wf.getframerate(),
+                    output = True,
+                    output_device_index = RESPEAKER_INDEX)
+
+    # read data (based on the chunk size)
+    data = wf.readframes(chunk)
+    # play stream (looping from beginning of file to the end)
+    while data:
+        # writing to the stream is what *actually* plays the sound.
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+    # cleanup stuff.
+    stream.close()    
+    p.terminate()
 
 def get_settings():
     full_file_path = Path(__file__).parent.joinpath('settings.yaml')
@@ -69,6 +104,8 @@ def startup():
     sleep(4)
  
     redled.off()
+
+    play_wav(INTROWAV)
  
 
 try:
